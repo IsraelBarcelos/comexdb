3bfbad173d29020837186a02b93e97e1a1a8e353
= <=>
2022-06-08 09:16:26 -0300

    Cliente com relatorio

--- src/main/java/br/com/alura/dao/ClienteDAO.java
+++ src/main/java/br/com/alura/dao/ClienteDAO.java
@@ -6,39 +6,55 @@ import javax.persistence.EntityManager;
 
 public class ClienteDAO {
 
-  private EntityManager em;
+    private EntityManager em;
 
-  public ClienteDAO(EntityManager em) {
-    this.em = em;
-  }
+    public ClienteDAO(EntityManager em) {
+        this.em = em;
+    }
 
-  public void cadastra(Cliente cliente) {
-    em.persist(cliente);
-  }
+    public void cadastra(Cliente cliente) {
+        em.persist(cliente);
+    }
 
-  public Cliente buscaPorId(Long id) {
-    return em.find(Cliente.class, id);
-  }
+    public Cliente buscaPorId(Long id) {
+        return em.find(Cliente.class, id);
+    }
 
-  public void remove(Cliente cliente) {
-    em.remove(cliente);
-  }
+    public void remove(Cliente cliente) {
+        em.remove(cliente);
+    }
 
-  public void atualiza(Cliente cliente) {
-    em.merge(cliente);
-  }
+    public void atualiza(Cliente cliente) {
+        em.merge(cliente);
+    }
 
-  public List<Cliente> listaTodos() {
-    return this.em.createQuery("select c from Cliente c", Cliente.class)
-      .getResultList();
-  }
+    public List<Cliente> listaTodos() {
+        return this.em.createQuery("select c from Cliente c", Cliente.class)
+                .getResultList();
+    }
 
-  public List<Cliente> listaPorNome(String nome) {
-    return this.em.createQuery(
-        "select c from Cliente c where c.nome = :nome",
-        Cliente.class
-      )
-      .setParameter("nome", nome)
-      .getResultList();
-  }
+    public List<Cliente> listaPorNome(String nome) {
+        return this.em.createQuery(
+                "select c from Cliente c where c.nome = :nome",
+                Cliente.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    private List<Cliente> listaClientesQueTiveramPeloMenosUmPedido() {
+        return this.em.createQuery(
+                "select c from Cliente c where c.pedidos is not empty",
+                Cliente.class)
+                .getResultList();
+    }
+
+    public void relatorioCliente() {
+        List<Cliente> clientes = this.listaClientesQueTiveramPeloMenosUmPedido();
+        for (Cliente cliente : clientes) {
+            System.out.print("\n" + cliente.getNome());
+            System.out
+                    .println(" realizou " + new PedidoDAO(em).quantidadeDePedidosDeUmCliente(cliente) + " pedido(s)");
+        }
+    }
+
 }
--- src/main/java/br/com/alura/mains/MainClienteDAO.java
+++ src/main/java/br/com/alura/mains/MainClienteDAO.java
@@ -1,50 +1,50 @@
 package br.com.alura.mains;
 
-import antlr.collections.List;
 import br.com.alura.dao.ClienteDAO;
 import br.com.alura.models.Cliente;
 import br.com.alura.models.Endereco;
 import br.com.alura.utils.JPAUtil;
-import java.util.Optional;
 import javax.persistence.EntityManager;
 
 public class MainClienteDAO {
 
-  public static void main(String[] args) {
-    Cliente cliente1 = new Cliente(
-      "Maria",
-      "123.456.789-10",
-      "(11) 98765-4321",
-      new Endereco("Rua dos Bobos", 0, "Apto. 101", "centro", "são paulo", "sp")
-    );
-    Cliente cliente2 = new Cliente(
-      "Joao",
-      "123.456.789-10",
-      "(11) 98765-4321",
-      new Endereco("Rua dos Bobos", 0, "Apto. 101", "centro", "são paulo", "sp")
-    );
-    Cliente cliente3 = new Cliente(
-      "Matias",
-      "123.456.789-10",
-      "(11) 98765-4321",
-      new Endereco("Rua dos Bobos", 0, "Apto. 101", "centro", "são paulo", "sp")
-    );
+        public static void main(String[] args) {
+                Cliente cliente1 = new Cliente(
+                                "Maria",
+                                "123.456.789-10",
+                                "(11) 98765-4321",
+                                new Endereco("Rua dos Bobos", 0, "Apto. 101", "centro", "são paulo", "sp"));
+                Cliente cliente2 = new Cliente(
+                                "Joao",
+                                "123.456.789-10",
+                                "(11) 98765-4321",
+                                new Endereco("Rua dos Bobos", 0, "Apto. 101", "centro", "são paulo", "sp"));
+                Cliente cliente3 = new Cliente(
+                                "Matias",
+                                "123.456.789-10",
+                                "(11) 98765-4321",
+                                new Endereco("Rua dos Bobos", 0, "Apto. 101", "centro", "são paulo", "sp"));
 
-    EntityManager em = JPAUtil.getEntityManager();
+                EntityManager em = JPAUtil.getEntityManager();
 
-    em.getTransaction().begin();
+                em.getTransaction().begin();
 
-    ClienteDAO clienteDao = new ClienteDAO(em);
+                ClienteDAO clienteDAO = new ClienteDAO(em);
 
-    clienteDao.cadastra(cliente1);
-    clienteDao.cadastra(cliente2);
-    clienteDao.cadastra(cliente3);
+                /*
+                 * clienteDAO.cadastra(cliente1);
+                 * clienteDAO.cadastra(cliente2);
+                 * clienteDAO.cadastra(cliente3);
+                 * 
+                 * clienteDAO
+                 * .listaPorNome(cliente3.getNome())
+                 * .stream()
+                 * .forEach(System.out::println);
+                 */
 
-    clienteDao
-      .listaPorNome(cliente3.getNome())
-      .stream()
-      .forEach(System.out::println);
+                em.getTransaction().commit();
 
-    em.getTransaction().commit();
-  }
+                clienteDAO.relatorioCliente();
+
+        }
 }
